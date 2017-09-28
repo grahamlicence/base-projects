f0801a595b0d356e62db9dcddded353c06b1cab5
Graham Licence <git@grahamlicence.co.uk>
2017-09-28 11:36:33 +0100

    Updates loaders

--- webpack-react/config/webpack/webpack.loaders.js
+++ webpack-react/config/webpack/webpack.loaders.js
@@ -1,44 +1,47 @@
 module.exports = [
     {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader'
-      }
+        test: /\.js$/,
+
+        // if a module uses es6, include using:
+        // exclude: /node_modules\/(?!(MY-MODULE|ANOTHER-ONE)\/).*/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+        },
     },
     {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'file-loader'
+        exclude: /(node_modules)/,
+        loader: 'file-loader',
     },
     {
         test: /\.(woff|woff2)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?prefix=font/&limit=5000'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?prefix=font/&limit=5000',
     },
     {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
     },
     {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
     },
     {
         test: /\.gif/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?limit=30000&mimetype=image/gif'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?limit=30000&mimetype=image/gif',
     },
     {
         test: /\.jpg/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?limit=30000&mimetype=image/jpg'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?limit=30000&mimetype=image/jpg',
     },
     {
         test: /\.png/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'url-loader?limit=30000&mimetype=image/png'
+        exclude: /(node_modules)/,
+        loader: 'url-loader?limit=30000&mimetype=image/png',
     }
 ];
