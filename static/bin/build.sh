d45dd62d278d21101af8e78165ba10e41e9cdce6
Graham Licence <git@grahamlicence.co.uk>
2017-11-20 16:30:25 +0000

    Updates npm scripts

--- static/bin/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -R templates/. dist/
-nodemon -e scss -x "npm run sass && npm run js" & node server.js
--- static/bin/sass.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-node-sass --include-path src src/main.scss   dist/assets/main.css
