cb84d8bb0e1ac39873e909efae44528e50c4b159
Graham Licence <git@grahamlicence.co.uk>
2018-03-22 13:05:46 +0000

    config tidy

--- webpack-react/config/webpack/webpack.config.prod.js
+++ webpack-react/config/webpack/webpack.config.prod.js
@@ -10,56 +10,8 @@ const CSSNano = require('cssnano');
 const packageJSON = require('../../package.json');
 
 const loaders = require('./webpack.loaders');
+
 const dir = fs.realpathSync(process.cwd());
-
-// local css modules
-// loaders.push({
-//     test: /[\/\\]src[\/\\].*\.css/,
-//     exclude: /(node_modules|bower_components|public)/,
-//     use: [
-//         MiniCssExtractPlugin.loader,
-//         'style-loader',
-//         'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-//     ],
-// });
-
-// local scss modules
-// loaders.push({
-//     test: /[\/\\]src[\/\\].*\.scss/,
-//       // test: /\.scss$/,
-//     exclude: /(node_modules|dist|build)/,
-//     use: [
-//         {
-//             loader: 'css-loader',
-//             options: {
-//                 sourceMap: true,
-//                 minimize: true,
-//             },
-//         },
-//         {
-//             loader: 'postcss-loader',
-//             options: {
-//                 plugins: () => [
-//                     autoprefixer({
-//                         browsers: [
-//                             '>1%',
-//                             'last 4 versions',
-//                             'Firefox ESR',
-//                             'not ie < 9', // React doesn't support IE8 anyway
-//                         ],
-//                     flexbox: 'no-2009',
-//                 })],
-//                 sourceMap: true,
-//             },
-//         },
-//         {
-//             loader: 'sass-loader',
-//             options: {
-//                 sourceMap: true,
-//             },
-//         },
-//     ],
-// });
 
 // sass
 loaders.push({
@@ -75,35 +27,38 @@ loaders.push({
             }
         },
         {
+            loader: 'postcss-loader',
+            options: {
+                plugins: () => [
+                    autoprefixer({
+                        browsers: [
+                            '>1%',
+                            'last 4 versions',
+                            'Firefox ESR',
+                            'not ie < 9', // React doesn't support IE8 anyway
+                        ],
+                    flexbox: 'no-2009',
+                })],
+                sourceMap: true,
+            },
+        },
+        {
             loader: 'sass-loader',
             options: {
                 sourceMap: true,
-                precision: 8,
-                data: '$ENV: ' + 'PRODUCTION' + ';'
             },
         },
         {
             loader: 'sass-resources-loader',
             options: {
-
-                // array of paths to sass resources (previously @import in each file)
                 resources: [
                     path.resolve(dir, 'src/sass/variables.scss'),
                     path.resolve(dir, 'src/sass/mixins.scss'),
                 ]
-            }
+            },
         },
     ],
 });
-
-// global css files
-// loaders.push({
-//     test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
-//     use: [
-//         MiniCssExtractPlugin.loader,
-//         'css-loader',
-//     ],
-// });
 
 module.exports = {
     context: path.resolve(dir, 'src'),
@@ -134,12 +89,6 @@ module.exports = {
 
         new CopyWebpackPlugin([
             { from: './images', to: './images' },
-            {
-                ignore: [
-                    '.DS_Store',
-                    '.gitkeep'
-                ]
-            }
         ]),
 
         new MiniCssExtractPlugin({
